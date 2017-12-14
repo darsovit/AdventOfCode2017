b25ff4fa1d463bec36cac1ebb6fa219501ad0081
Jonathan Cameron <darsovit@gmail.com>
2017-12-14 08:22:10 -0700

    Advent of Code 2017, Day 13 solution
    Updates the solution to add detection of collision
    in step 1 (when there's delay!). It could be improved.
    
    http://adventofcode.com/2017/day/13

--- Day13/Day13.py
+++ Day13/Day13.py
@@ -1,13 +1,19 @@
 #!python
 
-def calculate_collision( step, range, delay ):
-    print( "calculate_collision(", step, ",", range, ",", delay, "), detects position of scanner at:", (step+delay) % ( (range-1)*2) )
-    return ( (step+delay) % ( (range-1) * 2 ) ) == 0
+def calculate_collision_with_scanner( time, range ):
+#    print( "calculate_collision(", step, ",", range, ",", delay, "), detects position of scanner at:", (step+delay) % ( (range-1)*2) )
+    return ( time % ( (range-1) * 2 ) ) == 0
+
+def calculate_collision( step, state, delay ):
+    if step in state['layer']:
+        return calculate_collision_with_scanner( step+delay, state['layer'][step] )
+    else:
+        return false
 
 def calculate_cost( step, state, delay ):
     if step in state['layer']:
-        if calculate_collision( step, state['layer'][step], delay ):
-            return step * state['layer'][step]
+        if calculate_collision_with_scanner( step+delay, state['layer'][step] ):
+            return step * state['layer'][step] + delay # Add delay to cost to force caught by first scanner to have a cost
     return 0
 
 def parse_line( line, state ):
@@ -40,8 +46,8 @@ while ( cost > 0 ):
     for step in range( 0, max(state['layer'].keys() )+1 ):
         cost = calculate_cost( step, state, delay )
         if cost > 0:
-            print( "delay: ", delay, " leads to failure in step: ", step )
+            #print( "delay: ", delay, " leads to failure in step: ", step )
             break
 
-# 156052 is not correct
+# 156052 is not correct, because we also can't be caught by the first scanner (which reported 0 cost before)
 print( "delay to leave out without any cost: ", delay )
